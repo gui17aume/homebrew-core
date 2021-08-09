d090d339192c30e44926d18e413edd10a70d6f99
Guillaume Roche-Chosson <grcvoodoorush@gmail.com>
2021-08-10 00:30:24 +0200

    XCFrameworkNow 0.2.1

--- Formula/xcframework-now.rb
+++ Formula/xcframework-now.rb
@@ -1,8 +1,8 @@
 class XcframeworkNow < Formula
   desc "Utility for transforming a given library or framework into an XCFramework, generating the arm64 simulator slices if missing."
   homepage "https://github.com/gui17aume/xcframework-now"
-  url "https://github.com/gui17aume/xcframework-now/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "d55747a30c8221b2f4c4be535b3e6037ad403ccaa45176c5f144bdbd132e24b1"
+  url "https://github.com/gui17aume/xcframework-now/archive/refs/tags/0.2.1.tar.gz"
+  sha256 "f844b9b04470c11d5d92e42d50580b8c2c917156e6b1f656995f0a991acdf2c7"
   license "MIT"
 
   depends_on xcode: ["11.0", :build]
@@ -10,7 +10,7 @@ class XcframeworkNow < Formula
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox"
     system "install", "-d", "#{prefix}/bin"
-	  system "install", ".build/release/xcframework-now", "#{prefix}/bin"
+    system "install", ".build/release/xcframework-now", "#{prefix}/bin"
   end
 
   test do
